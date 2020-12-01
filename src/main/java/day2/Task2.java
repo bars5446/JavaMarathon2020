427b19c2d427db6c22d2103843c34a9db4ef1ea7
Edgar Barsegyan <bars5446@yandex.ru>
2020-12-01 19:40:16 +0300

    Add Day2 solution

--- src/main/java/day2/Task2.java
+++ src/main/java/day2/Task2.java
@@ -1,7 +1,21 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int leftEdge = scanner.nextInt();
+        int rightEdge = scanner.nextInt();
 
+            if (leftEdge < rightEdge) {
+                for (int i = leftEdge + 1; i < rightEdge ; i++) {
+                    if (i % 5 == 0 && i % 10 != 0) {
+                        System.out.print(i + " ");
+                    }
+                }
+            } else {
+                System.out.println("Некорректный ввод");
+            }
     }
 }
--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,22 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int leftEdge = scanner.nextInt();
+        int rightEdge = scanner.nextInt();
+        int a = leftEdge + 1;
+        if (leftEdge < rightEdge) {
+            while (a < rightEdge) {
+                if (a % 5 == 0 && a % 10 != 0) {
+                    System.out.print(a + " ");
+                }
+                a++;
+            }
+        } else {
+            System.out.println("Некорректный ввод");
+        }
     }
 }
