b140671ba832b62d2a99d26906833bcb94cf9e25
Edgar Barsegyan <bars5446@yandex.ru>
2020-12-04 00:02:09 +0300

    Add Day4 solution

--- src/main/java/day4/Task4.java
+++ src/main/java/day4/Task4.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Task4 {
     public static void main(String[] args) {
-        int[] array = new int[100];
+        int[] array = new int[100000];
         Random random = new Random();
 
         for (int i = 0; i < array.length; i++) {
@@ -12,15 +12,21 @@ public class Task4 {
         }
 
         int index = 0;
-        int sum = 0;
-        for (int i = 0; i < array.length - 2; i++){
-            int a = array[i] + array[i + 1] + array [i + 2];
-            if (a > sum) {
-                sum = a;
+        int sum2 = 0;
+
+        for (int i = 0; i < array.length - 49; i++){
+            int sum = 0;
+            for (int j = i; j < i + 50; j++ ){
+                sum += array[j];
+            }
+            if (sum > sum2) {
+                sum2 = sum;
                 index = i;
             }
+
         }
-        System.out.println("Сумма тройки: " + sum);
+
+        System.out.println("Сумма тройки: " + sum2);
         System.out.println("Индекс первого элемента тройки: " + index);
     }
 }
