87b0f2f9d6acd3fbf7fb3c3d0cf8069b303c8117
Edgar Barsegyan <bars5446@yandex.ru>
2020-12-13 13:55:11 +0300

    Add Day12 Task2 fix

--- src/main/java/day12/Task2.java
+++ src/main/java/day12/Task2.java
@@ -6,15 +6,11 @@ public class Task2 {
     public static void main(String[] args) {
         ArrayList<Integer> numList = new ArrayList<>();
 
-        for (int i = 0; i < 30; i++) {
-            if (i % 2 == 0 && i != 0) {
-                numList.add(i);
-            }
-        }
-        System.out.println(numList.toString());
 
-        for (int i = 300; i < 350; i++) {
-            if (i % 2 == 0 && i != 300) {
+        for (int i = 0; i < 350; i++) {
+            if (i % 2 == 0 && (i > 0 && i < 30)) {
+                numList.add(i);
+            } if (i % 2 == 0 && i > 300) {
                 numList.add(i);
             }
         }
