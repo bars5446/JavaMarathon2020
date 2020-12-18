04517144d9619a77103dda087e122695b5240709
Edgar Barsegyan <bars5446@yandex.ru>
2020-12-18 22:56:07 +0300

    Add Day18 Task 1 and 2 solution

--- src/main/java/day18/Task1.java
+++ src/main/java/day18/Task1.java
@@ -2,6 +2,14 @@ package day18;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        int[] numbers = {1, 2, 3, 4};
+        System.out.println(recursionSum(numbers, 0));
+    }
+    public static int recursionSum(int[] arrayNum, int index) {
+        if (index < arrayNum.length) {
+            return arrayNum[index] + recursionSum(arrayNum, index + 1);
+        } else {
+            return 0;
+        }
     }
 }
--- src/main/java/day18/Task2.java
+++ src/main/java/day18/Task2.java
@@ -1,7 +1,21 @@
 package day18;
 
 public class Task2 {
-    public static void main(String[] args) {
+    public static int num_7_sum = 0;
 
+    public static void main(String[] args) {
+        System.out.println(count7(717771237));
+    }
+    // Работает, но я не уверен, что правильно было делать через статическую переменную
+    public static int count7(int num) {
+        if (num % 10 == 7) {
+            num_7_sum++;
+            count7(num / 10);
+        } else if(num / 10 < 1) {
+            return 0;
+        } else {
+            count7(num / 10);
+        }
+        return num_7_sum;
     }
 }
